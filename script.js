32594baa1c2610079bf87c6e454913eee93b8113
David Aylward <davidaylward@Davids-Mac-mini.lan>
2022-03-30 22:18:57 -0400

    done

--- script.js
+++ script.js
@@ -1,16 +1,49 @@
 const submit = document.getElementById('wthForm')
-var inputEl = document.querySelector("#inputDefault");
+var inputEl = document.querySelector("#inputDefault")
+var rainEl = document.querySelector('#inputRain')
+var tempEl = document.querySelector('#temparatureEl')
+var umbrellaEl = document.querySelector('#umbrellaEl')
+var jacketEl = document.querySelector('#jacketEl')
+var checkBox = document.querySelector('#isItC')
+const cToF = celsius => celsius * 9/5 + 32;
+const fToC = fahrenheit => (fahrenheit - 32) * 5/9;
+const celcius = fToC(inputEl.value);
 
-var getTemp = function(tempInput){
-if (tempInput < 70) {
-    console.log('heyman')
+
+var getTemp = function(){
+if (inputEl.value < 70) {
+    jacketEl.innerHTML = 'Wear a jacket!'
+} else {
+    jacketEl.innerHTML = 'No jacket needed!'
 }
 }
+
+var getRain = function(){
+    if (rainEl.value.toLowerCase() == 'yes') {
+        umbrellaEl.innerHTML = 'Bring an umbrella'
+    }
+    else {
+        umbrellaEl.innerHTML = 'No umbrella needed!'
+    }
+    }
+
+    var convertTemp = function(){
+        const farenheit = cToF(inputEl.value);
+        const celcius = fToC(inputEl.value);
+        if (checkBox.checked == true) {
+        tempEl.innerHTML = farenheit;
+        } else {
+            tempEl.innerHTML = celcius
+        }
+    }
+
 
 var handleSubmission = function (event) {
-    event.preventDefault();
+    event.preventDefault()
+    getTemp()
+    getRain()
+    convertTemp();
     
 }
 submit.addEventListener('submit', handleSubmission)
 
-console.log('hello')
